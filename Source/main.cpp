7e8b2dd97848c9f377e464b8801e9e16bb97c67e
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2021-10-24 19:29:49 +0800

    Horizon_xpl AP

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,87 +6,41 @@
 //
 
 #include <cstring>
-#include "XPLMDisplay.h"
-#include "XPLMGraphics.h"
+#include "XPLMMenus.h"
+#include "XPStandardWidgets.h"
+#include "XPWidgets.h"
 
-// An opaque handle to the window we will create.
-static XPLMWindowID g_window;
+static XPWidgetID gHorizonXplMainWindow = NULL;
+static XPWidgetID gHorizonXplRTAttitude[3];
+static XPWidgetID gHorizonXplTGAttitude[3];
 
-// Callbacks we will register when we create our window.
-void draw_hello_world(XPLMWindowID in_window_id, void *in_refcon);
-inline int dummy_mouse_handler(XPLMWindowID in_window_id,
-                               int x, int y, int is_down,
-                               void *in_refcon) {
-    return 0;
-}
-inline void dummy_key_handler(XPLMWindowID in_window_id,
-                              char key, XPLMKeyFlags flags, char virtual_key,
-                              void *in_refcon, int losing_focus) {
-    return;
-}
-inline XPLMCursorStatus dummy_cursor_status_handler(XPLMWindowID in_window_id,
-                                                    int x, int y,
-                                                    void *in_refcon) {
-    return xplm_CursorDefault;
-}
-inline int dummy_wheel_handler(XPLMWindowID in_window_id,
-                               int x, int y, int wheel, int clicks,
-                               void *in_refcon) {
-    return 0;
-}
+static void horizonXplCreateMainWindow(int, int);
+static void horizonXplMenuHandler(void*, void*);
+static int horizonXplWidgetHandler(XPWidgetMessage, XPWidgetID, long, long);
 
 PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc) {
+    XPLMMenuID menuId;
+    int subMenuItem;
+
     strcpy(outName, "Horizon_xpl");
     strcpy(outSig, "jichen.Horizon_xpl");
-    strcpy(outDesc, "An auto pilot plug-in for X-Plane 11.");
+    strcpy(outDesc, "An autopilot plugin for X-Plane 11.");
 
-    XPLMCreateWindow_t params;
-    params.structSize = sizeof(params);
-    params.visible = 1;
-    params.drawWindowFunc = draw_hello_world;
-    // Note on "dummy" handlers:
-    // Even if we don't want to handle these events, we have to register a
-    // "do-nothing" callback for them.
-    params.handleMouseClickFunc = dummy_mouse_handler;
-#if defined(XPLM300)
-    params.handleRightClickFunc = dummy_mouse_handler;
-#endif /* XPLM300 */
-    params.handleKeyFunc = dummy_key_handler;
-    params.handleCursorFunc = dummy_cursor_status_handler;
-    params.handleMouseWheelFunc = dummy_wheel_handler;
-    params.refcon = nullptr;
-#if defined(XPLM301)
-    params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
-#endif /* XPLM301 */
-#if defined(XPLM300)
-    params.layer = xplm_WindowLayerFloatingWindows;
-#endif /* XPLM300 */
-    // Set the window's initial bounds
-    // Note that we're not guaranteed that the main monitor's lower left is at (0, 0)...
-    // We'll need to query for the global desktop bounds!
-    int left, top, right, bottom;
-    XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
-    params.top = top - 50;
-    params.right = right - 50;
-    params.bottom = params.top - 200;
-    params.left = params.right - 200;
+    subMenuItem = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "Horizon_xpl", NULL, 1);
+    menuId = XPLMCreateMenu("Horizon_xpl", XPLMFindPluginsMenu(), subMenuItem,
+                            horizonXplMenuHandler, NULL);
+    XPLMAppendMenuItem(menuId, "AP", (void*) "AP", 1);
 
-    g_window = XPLMCreateWindowEx(&params);
-
-    // Position the window as a "free" floating window, which the user can drag around.
-    XPLMSetWindowPositioningMode(g_window, xplm_WindowPositionFree, -1);
-    // Limit resizing our window: maintain a minimum width/height of 100 boxels
-    // and a max width/height of 300 boxels.
-    XPLMSetWindowResizingLimits(g_window, 100, 100, 300, 300);
-    XPLMSetWindowTitle(g_window, "Sample Window");
-
-    return g_window != nullptr;
+    return 1;
 }
 
 PLUGIN_API void XPluginStop(void) {
-    // Since we created the window, we'll be good citizens and clean it up.
-    XPLMDestroyWindow(g_window);
-    g_window = nullptr;
+    if (gHorizonXplMainWindow) {
+        XPDestroyWidget(gHorizonXplMainWindow, 1);
+        gHorizonXplMainWindow = NULL;
+        memset(gHorizonXplRTAttitude, NULL, 3);
+        memset(gHorizonXplTGAttitude, NULL, 3);
+    }
 }
 
 PLUGIN_API void XPluginDisable(void) {
@@ -97,27 +51,128 @@ PLUGIN_API int XPluginEnable(void) {
     return 1;
 }
 
-PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void *inParam) {
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID /*inFrom*/, int /*inMsg*/, void* /*inParam*/) {
     return;
 }
 
-void draw_hello_world(XPLMWindowID in_window_id, void *in_refcon) {
-    // Mandatory: We *must* set the OpenGL state before drawing
-    // (we can't make any assumptions about it).
-    XPLMSetGraphicsState(0, // no fog
-                         0, // 0 texture units
-                         0, // no lighting
-                         0, // no alpha testing
-                         1, // do alpha blend
-                         1, // do depth testing
-                         0); // no depth writing
+static void horizonXplCreateMainWindow(int width, int height) {
+    static const char *attitude[3] = {"roll", "Pitch", "YAW"};
+    int left, top, right, bottom;
+    int row, column;
 
-    int l, t, r, b;
-    XPLMGetWindowGeometry(in_window_id, &l, &t, &r, &b);
+    // calculate Main Window location
+    XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
+    top -= 50;
+    right -= 50;
+    bottom = top - height;
+    left = right - width;
+    // create Main Window
+    gHorizonXplMainWindow = XPCreateWidget(left, top, right, bottom,
+                                           1, // visible
+                                           "Horizon_xpl AP", // description
+                                           1, // root
+                                           NULL, // must be NULL for root
+                                           xpWidgetClass_MainWindow);
+    XPSetWidgetProperty(gHorizonXplMainWindow, xpProperty_MainWindowType,
+                        xpMainWindowStyle_Translucent);
+    XPSetWidgetProperty(gHorizonXplMainWindow, xpProperty_MainWindowHasCloseBoxes, 1);
 
-    float col_white[] = {1.0, 1.0, 1.0}; // red, green, blue
+    // roll Pitch YAW
+    row = top - 30;
+    column = left;
+    for (int i = 0; i < 3; ++i) {
+        int step = width / 4;
+        int lb = column + (i + 1) * step + 10;
+        int rb = column + (i + 2) * step - 10;
+        int tb = row;
+        int bb = tb - 12;
+        XPCreateWidget(lb, tb, rb, bb,
+                       1, // visible
+                       attitude[i], // description
+                       0, // not root
+                       gHorizonXplMainWindow, // belongs to Main Window
+                       xpWidgetClass_Caption);
+    }
 
-    XPLMDrawString(col_white, l + 10, t - 20,
-                   (char *) "Hello World!",
-                   nullptr, xplmFont_Proportional);
+    // Realtime
+    row -= 20;
+    column = left;
+    for (int i = -1; i < 3; ++i) {
+        int step = width / 4;
+        int lb = column + (i + 1) * step + 10;
+        int rb = column + (i + 2) * step - 10;
+        int tb = row;
+        int bb = tb - 12;
+        char *desc = NULL;
+        XPWidgetID tmp = NULL;
+        if (i < 0) {
+            desc = (char*) "Realtime";
+        } else {
+            desc = (char*) "   0.0";
+        }
+        tmp = XPCreateWidget(lb, tb, rb, bb,
+                             1, // visible
+                             desc, // description
+                             0, // not root
+                             gHorizonXplMainWindow, // belongs to Main Window
+                             xpWidgetClass_Caption);
+        if (i >= 0) {
+            XPSetWidgetProperty(tmp, xpProperty_CaptionLit, 1);
+            gHorizonXplRTAttitude[i] = tmp;
+        }
+    }
+
+    // Target
+    row -= 20;
+    column = left;
+    for (int i = -1; i < 3; ++i) {
+        int step = width / 4;
+        int lb = column + (i + 1) * step + 10;
+        int rb = column + (i + 2) * step - 10;
+        int tb = row;
+        int bb = tb - 12;
+        char *desc = NULL;
+        XPWidgetID tmp = NULL;
+        if (i < 0) {
+            desc = (char*) "Target";
+        } else if (i < 2) {
+            desc = (char*) "   0.0";
+        } else {
+            desc = (char*) " n/a";
+        }
+        tmp = XPCreateWidget(lb, tb, rb, bb,
+                             1, // visible
+                             desc, // description
+                             0, // not root
+                             gHorizonXplMainWindow, // belongs to Main Window
+                             xpWidgetClass_Caption);
+        if (i >= 0) {
+            gHorizonXplTGAttitude[i] = tmp;
+        }
+    }
+
+    XPAddWidgetCallback(gHorizonXplMainWindow, horizonXplWidgetHandler);
+}
+
+static void horizonXplMenuHandler(void* /*inMenuRef*/, void *inItemRef) {
+    if (strcmp((char*) inItemRef, "AP") == 0) {
+        if (!gHorizonXplMainWindow) {
+            horizonXplCreateMainWindow(320, 560);
+        } else {
+            if (!XPIsWidgetVisible(gHorizonXplMainWindow)) {
+                XPShowWidget(gHorizonXplMainWindow);
+            }
+        }
+    }
+}
+
+static int horizonXplWidgetHandler(XPWidgetMessage inMessage, XPWidgetID inWidget,
+                                   long /*inParam1*/, long /*inParam2*/) {
+    if (inMessage == xpMessage_CloseButtonPushed) {
+        if (inWidget == gHorizonXplMainWindow) {
+            XPHideWidget(gHorizonXplMainWindow);
+        }
+        return 1;
+    }
+    return 0;
 }
